c16946ebb4947ac0f2f02e9d2e7bac3e87c1d4e0
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2018-12-22 17:53:58 +0300

    Update Vote.sol

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.5.0;
 
 interface TokenAction {
-    function allowance(address owner, address spender) external view returns (uint);
     function transferFrom(address from, address to, uint value) external returns (bool);
     function balanceOf(address who) external view returns (uint);
 }
@@ -17,12 +16,12 @@ contract Vote {
     //called erc20 contract
     TokenAction erc20Communication;
 
-    modifier votingProcesses() {
+    modifier isActive() {
         require(votingDeadline > now, "voting event is ended");
         _;
     }
 
-    modifier votingEnded() {
+    modifier isEnded() {
         require(votingDeadline <= now, "voting event still lasts");
         _;
     }
@@ -59,8 +58,8 @@ contract Vote {
     @param candidate candidate in bytes32 form, checked by "require".
     @return true, if VM processed tx successfully.
      */
-    function registerVoter(bytes32 candidate) public votingProcesses returns (bool) {
-        require(votersData[msg.sender].registered == false && _validateCandidate(candidate) == true, "you are registered");
+    function registerVoter(bytes32 candidate) public isActive returns (bool) {
+        require(votersData[msg.sender].registered == false && _isValidCandidate(candidate) == true, "you are registered");
         votersList.push(msg.sender);
         votersData[msg.sender] = VoterInfo(true, candidate, 0);
         return true;
@@ -71,7 +70,7 @@ contract Vote {
     @param amount an amount of giving voices for a candidate, should be greater than 0.
     @return true, if VM processed tx successfully.
      */
-    function voteForYourCandidate(uint amount) public votingProcesses returns (bool) {
+    function voteForYourCandidate(uint amount) public isActive returns (bool) {
         require(amount > 0 && votersData[msg.sender].registered == true, "incorrect data"); //нужен ли булеан?
         bytes32 candidate = votersData[msg.sender].choice;
         votesForCandidates[candidate] += amount;
@@ -84,11 +83,11 @@ contract Vote {
     @dev method which finalize the voting event by honoring winners with losers tokens. Called only after deadline.
     @return winner name
      */
-    function honorWinners() public votingEnded returns (bytes32) { 
+    function honorWinners() public isEnded returns (bytes32) { 
         bytes32 _winnerName = _findWinner();
         uint winnersPrey = erc20Communication.balanceOf(address(this)) - totalVotesForCandidate(_winnerName);
         for (uint i = 0; i < votersList.length; i++) {
-            if (votersData[votersList[i]].choice ==_winnerName) {
+            if (votersData[votersList[i]].choice == _winnerName) {
                 votersData[votersList[i]].voices += (winnersPrey * votersData[votersList[i]].voices)/totalVotesForCandidate(_winnerName);              
             } else {
                 votersData[votersList[i]].voices = 0;
@@ -111,7 +110,7 @@ contract Vote {
     @param candidate *
     @return boolean. True if candidate was found, else - false.
      */
-    function _validateCandidate(bytes32 candidate) internal view returns (bool) {
+    function _isValidCandidate(bytes32 candidate) internal view returns (bool) {
         for (uint i = 0; i < candidates.length; i++) {
             if (candidates[i] == candidate) {
                 return true;
@@ -124,7 +123,7 @@ contract Vote {
     @dev an internal method used by honorWinners method to find a winner.
     @return winner name.
      */
-    function _findWinner() internal view votingEnded returns (bytes32) {
+    function _findWinner() internal view isEnded returns (bytes32) {
         bytes32 winner = "";
         uint maxVotes = 0;
         for (uint i = 0; i < candidates.length; i++) {
