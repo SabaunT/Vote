b1b8c400083ce36f53d6059118d90cfd5b3ca9d7
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2018-12-25 16:37:29 +0300

    migration for a commit-reveal contract
    change migration using commented script to test Vote.sol contract

--- migrations/2_vote.js
+++ migrations/2_vote.js
@@ -1,7 +1,10 @@
 const TestToken = artifacts.require("./TestToken.sol");
-const Vote = artifacts.require("./Vote.sol");
+//const Vote = artifacts.require("./Vote.sol");
+const VoteCR = artifacts.require("./VoteCR.sol");
 module.exports = function (deployer) {
     deployer.deploy(TestToken, "TestToken", "TT", 18).then(function () {
-        return deployer.deploy(Vote, 30, TestToken.address, ["0x436f6361436f6c61", "0x46616e7461"]); 
+        return deployer.deploy(VoteCR, 10, TestToken.address, ["0x436f6361436f6c61", "0x46616e7461"]); 
     });
 };
+
+//return deployer.deploy(Vote, 30, TestToken.address, ["0x..", "0x.."]);
